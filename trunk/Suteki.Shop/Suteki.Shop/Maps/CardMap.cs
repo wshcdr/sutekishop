59a671b41dfc457d13a83f063ab98c3a6f23ab71
mikehadlow <mikehadlow@42d4aba2-f048-0410-9719-5941d86a54b7>
2010-10-06 15:40:54 +0000

    Update script for latest DB schema changes.

--- trunk/Suteki.Shop/Suteki.Shop/Maps/CardMap.cs
+++ trunk/Suteki.Shop/Suteki.Shop/Maps/CardMap.cs
@@ -11,8 +11,8 @@ namespace Suteki.Shop.Maps
             Map(x => x.ExpiryYear);
             Map(x => x.Holder);
             Map(x => x.IssueNumber);
-            Map(x => x.Number);
-            Map(x => x.SecurityCode);
+            Map(x => x.Number).Length(500);
+            Map(x => x.SecurityCode).Length(500);
             Map(x => x.StartMonth);
             Map(x => x.StartYear);
 
