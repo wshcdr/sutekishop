1445c58e76c75d281ec5d1233019c6b7625c3465
mikehadlow <mikehadlow@42d4aba2-f048-0410-9719-5941d86a54b7>
2010-10-17 10:54:37 +0000

    Keep forgetting to up the version number :p

--- trunk/Suteki.Shop/Version.cs
+++ trunk/Suteki.Shop/Version.cs
@@ -2,5 +2,5 @@
 
 // Major-New architecture . Minor-New feature . Revision-Not used . Build-Svn repository checkin number
 
-[assembly: AssemblyVersion("2.0.1.442")]
-[assembly: AssemblyFileVersion("2.0.1.442")]
+[assembly: AssemblyVersion("2.0.1.448")]
+[assembly: AssemblyFileVersion("2.0.1.448")]
