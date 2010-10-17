6b71245de344936d769bc54ab2ceae56c10fa9ed
mikehadlow <mikehadlow@42d4aba2-f048-0410-9719-5941d86a54b7>
2010-10-17 10:59:47 +0000

    Fixed failing test :p

--- Suteki.Shop/Version.cs
+++ Suteki.Shop/Version.cs
@@ -2,5 +2,5 @@
 
 // Major-New architecture . Minor-New feature . Revision-Not used . Build-Svn repository checkin number
 
-[assembly: AssemblyVersion("2.0.1.448")]
-[assembly: AssemblyFileVersion("2.0.1.448")]
+[assembly: AssemblyVersion("2.0.1.449")]
+[assembly: AssemblyFileVersion("2.0.1.449")]
