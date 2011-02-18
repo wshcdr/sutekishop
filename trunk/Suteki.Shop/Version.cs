105ad449093274ef9a82165d4fb9c952335d7e41
mikehadlow@gmail.com <mikehadlow@gmail.com@42d4aba2-f048-0410-9719-5941d86a54b7>
2011-02-18 14:05:15 +0000

    Version bump to 2.2

--- trunk/Suteki.Shop/Version.cs
+++ trunk/Suteki.Shop/Version.cs
@@ -2,7 +2,8 @@
 
 // Major-New architecture . Minor-New feature . Revision-Not used . Build-Svn repository checkin number
 
-[assembly: AssemblyVersion("2.1.0.489")]
+[assembly: AssemblyVersion("2.2.0.490")]
 
+// 2.2 - Added Suteki.Shop.StockControl.AddIn
 // 2.1 - Upgraded to MVC3, .NET4, Using NuGet to manage OSS dependencies.
 // 2.0 - Changed to use NHibernate
